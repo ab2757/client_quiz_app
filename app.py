2cf50ed7d2162f773e7ce3168746de1ad48e96ed
ankit bansal <ab2757@gmail.com>
2025-05-23 13:12:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -74,4 +74,5 @@ def submit():
     return "✅ Response submitted and uploaded to Dropbox!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port,debug=True)
